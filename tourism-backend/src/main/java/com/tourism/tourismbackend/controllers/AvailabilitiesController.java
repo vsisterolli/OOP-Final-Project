d7b020b659ba847d2b1c133771324beab6aa07dd
enzopsm <enzopsm@hotmail.com>
2023-11-25 23:36:10 -0300

    Availabilities get/post routes created DestiniesController fixed application.properties fixed

--- tourism-backend/src/main/java/com/tourism/tourismbackend/controllers/AvailabilitiesController.java
+++ tourism-backend/src/main/java/com/tourism/tourismbackend/controllers/AvailabilitiesController.java
@@ -1,10 +1,10 @@
 package com.tourism.tourismbackend.controllers;
 
-import com.tourism.tourismbackend.dtos.DestinyDTO;
+import com.tourism.tourismbackend.dtos.AvailabilityDTO;
 import com.tourism.tourismbackend.dtos.RegisterDTO;
-import com.tourism.tourismbackend.models.Destinies;
+import com.tourism.tourismbackend.models.Availabilities;
 import com.tourism.tourismbackend.models.Users;
-import com.tourism.tourismbackend.repository.DestiniesRepository;
+import com.tourism.tourismbackend.repository.AvailabilitiesRepository;
 import com.tourism.tourismbackend.repository.UsersRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,25 +15,23 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/destinies")
-public class DestiniesController {
+@RequestMapping("/availabilities")
+public class AvailabilitiesController {
 
     @Autowired
-    private DestiniesRepository repository;
+    private AvailabilitiesRepository repository;
 
     @GetMapping
-    public List<Destinies> getUsers() {
+    public List<Availabilities> getUsers() {
         return repository.findAll();
     }
 
     @PostMapping
-    public ResponseEntity register(@RequestBody @Valid DestinyDTO data) {
-        if(this.repository.findByName(data.name()) != null)
-            return ResponseEntity.badRequest().build();
+    public ResponseEntity register(@RequestBody @Valid AvailabilityDTO data) {
 
-        Destinies newDestiny = new Destinies(data.name(), data.category());
+        Availabilities newAvailability = new Availabilities(data.iniDate(), data.endDate(), data.qntd_disponivel());
 
-        this.repository.save(newDestiny);
+        this.repository.save(newAvailability);
 
         return ResponseEntity.ok().build();
     }
