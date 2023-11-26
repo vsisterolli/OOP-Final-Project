8a6fcf31d04c75b6bc1e5e02ec5263bbf7a192af
enzopsm <enzopsm@hotmail.com>
2023-11-26 16:14:35 -0300

    image attribute moved from TravelPackages to Destinies

--- tourism-backend/src/main/java/com/tourism/tourismbackend/dtos/DestinyDTO.java
+++ tourism-backend/src/main/java/com/tourism/tourismbackend/dtos/DestinyDTO.java
@@ -2,4 +2,4 @@ package com.tourism.tourismbackend.dtos;
 
 import com.tourism.tourismbackend.models.enums.DestinyCategory;
 
-public record DestinyDTO(String name, DestinyCategory category) {}
+public record DestinyDTO(String name, DestinyCategory category, String image) {}
